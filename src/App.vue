fd8e0f3f0f1caa71e7252855e784ced56dea4229
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-11-26 19:32:36 -0300

    :tada: init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <span v-if="progress">progress is: {{ progress }}</span>
+    <form enctype="multipart/form-data" method="post" name="fileinfo">
+      <input type="file" @change="setFile">
+
+      <button @click.prevent="submit">submit</button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
 
 export default Vue.extend({
   name: 'app',
-  components: {
-    HelloWorld,
+
+  data () {
+    return {
+      file: null,
+      progress: 0
+    }
   },
+
+  // mounted () {
+  //   window.addEventListener('progress', this.handleProcess);
+  // },
+
+  methods: {
+    // handleProcess (event: any): void {
+    //   console.log(event);
+    // },
+
+    setFile (event: any): void {
+      this.file = event.target.files[0];
+    },
+
+    submit (): void {
+      let xhr = new XMLHttpRequest();
+      xhr.open('post', 'http://localhost:3000/');
+      xhr.setRequestHeader("Content-type", "multipart/form-data")
+
+      xhr.onload = () => console.log('loading');
+      xhr.onerror = () => console.log('error!');
+
+      xhr.upload.addEventListener('progress', (event: any) => {
+        if (!event.lengthComputable) return
+
+        const percent = event.loaded / event.total * 100;
+
+        if (percent === 100) {
+          this.progress = 0
+          return
+        }
+
+        this.progress = percent
+      })
+
+      let formData = new FormData();
+      formData.append('file', this.file!);
+
+      xhr.send(formData);
+    }
+  }
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
