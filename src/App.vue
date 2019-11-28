f3693bc3ce3c04134302a8671e201cc1dcd71bbc
Vinicius Azevedo <viniazvd@gmail.com>
2019-11-28 02:03:49 -0300

    :truck: move to mixin

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div v-if="upload && progress">
+    <div v-if="isUploading">
       <span>progress is: {{ progress }}</span>
-      <button @click="abortUpload">abort</button>
+      <button @click="$abortRequest">abort</button>
     </div>
 
     <form enctype="multipart/form-data" method="post" name="fileinfo">
       <input type="file" @change="setFile">
 
-      <button @click.prevent="submit">submit</button>
+      <button @click.prevent="$submit">submit</button>
     </form>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import progress from './progress'
+import VueCoeProgress from './VueCoeProgress'
 
 export default Vue.extend({
   name: 'app',
 
-  data () {
-    return {
-      progress: 0,
-      file: null as File | null,
-      upload: null as XMLHttpRequest | null
-    }
-  },
+  mixins: [ VueCoeProgress('http://localhost:3000/') ],
 
   methods: {
     setFile (event: Event): void {
@@ -35,39 +29,6 @@ export default Vue.extend({
       if (file && file.length) {
         this.file = (event.target as HTMLInputElement).files[0]
       }
-    },
-
-    abortUpload (): void {
-      this.upload.abort()
-
-      // reset
-      this.upload = null
-      this.progress = 0
-    },
-
-    submit (): void {
-      const self = this
-
-      function handleProgress (event: ProgressEvent): void {
-        if (!event.lengthComputable) return
-
-        const percent = event.loaded / event.total * 100;
-
-        self.progress = percent
-
-        if (percent === 100) return
-      }
-
-      function handleAbort () {
-        console.log('handle abort')
-      }
-
-      this.upload = progress({
-        url: 'http://localhost:3000/',
-        file: this.file,
-        abortFn: handleAbort,
-        progress: handleProgress
-      })
     }
   }
 });
